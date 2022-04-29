b78d54a736d3cedbf7ad8024b599e43850af2204
shahriyahosen <me.shahriyarhosen@gmail.com>
2022-04-29 15:20:31 +0600

    MongoDB connect and server get & post method  success😊

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
+const objectId = require("mongodb").ObjectId;
 const port = process.env.PORT || 5000;
 
 // Middleware
@@ -10,23 +13,47 @@ app.use(express.json());
 
 // async await function
 async function run() {
-    
-    // try catch finally
-    try {
-        
-    } finally {
-        
-    }
+  const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_KEY}@cluster0.hmt2r.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+  const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverApi: ServerApiVersion.v1,
+  });
+  // try catch finally
+  try {
+    await client.connect();
+    const inventoryCollection = client.db("warehouse").collection("inventory");
+
+    // Get Method to read all items
+    app.get("/inventory", async (req, res) => {
+      const query = {};
+      const cursor = inventoryCollection.find(query);
+      const inventory = await cursor.toArray();
+      res.send(inventory);
+    });
+    // -------------------------------------------
+
+    // Create POST Method items api
+    app.post("/inventory", async (req, res) => {
+      const newInventory = req.body;
+    //   console.log("New inventory adding ", newInventory);
+      const result = await inventoryCollection.insertOne(newInventory);
+      console.log(`Inventory insert with id: ${result.insertedId}`);
+      res.send({ result: "success" });
+    });
+    // --------------------------------------------------------
+  } finally {
+  }
 }
 // call function catch (console dir)
-run().catch(console.dir)
+run().catch(console.dir);
 // --------------------------------------------
 
 // http://localhost:5000/
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("DB Books Warehouse Server Running");
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`DB Books Warehouse Server port ${port}`);
 });
