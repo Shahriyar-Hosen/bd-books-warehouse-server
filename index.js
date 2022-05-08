c60fb7e4a5945c0858aaae7242be9d8b138f6d88
shahriyahosen <me.shahriyarhosen@gmail.com>
2022-05-08 12:28:12 +0600

    req pass decoded

--- index.js
+++ index.js
@@ -11,12 +11,22 @@ app.use(cors());
 app.use(express.json());
 // ---------------------------
 
+// JWT  token verify
 function verifyJWT(req, res, next) {
   const authHeader = req.headers.authorization;
   if (!authHeader) {
     return res.status(401).send({ message: "unauthorized access" });
   }
-  console.log("inside verifyJWT", authHeader);
+  const token = authHeader.split(" ")[1];
+  // verify a token symmetric
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(403).send({ message: "Forbidden access" });
+    }
+    console.log("decoded", decoded);
+    req.decoded = decoded;
+  });
+
   next();
 }
 
